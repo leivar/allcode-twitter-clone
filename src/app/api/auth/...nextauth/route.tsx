5a6fb06925c53eef499fa368e64909a0baa5a17e
Leif Ivar <leivarhh@hotmail.com>
2025-06-27 13:02:22 +0200

    for own use

--- src/app/api/auth/...nextauth/route.tsx
+++ src/app/api/auth/...nextauth/route.tsx
@@ -1,11 +1,11 @@
 import { PrismaAdapter } from "@next-auth/prisma-adapter";
 import GitHubProvider from "next-auth/providers/github";
 import NextAuth from "next-auth/next";
-import prisma from "@/lib/prisma"; 
+import prisma from "../../../../lib/prisma";
 
 export const authOptions = {
   secret: process.env.NEXTAUTH_SECRET,
-  adapter: PrismaAdapter(),
+  adapter: PrismaAdapter(prisma),
   providers: [
     GitHubProvider({
       clientId: process.env.GITHUB_ID ?? "",
